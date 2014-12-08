5185ea4296c330666a42326a5c2e164f1759bc59
tianlin shi <tianlinshi@gmail.com>
2014-12-08 11:15:32 +0800

    commit before git bisect

--- OnlineGibbsMedLDA/inc/HybridMedLDA.h
+++ OnlineGibbsMedLDA/inc/HybridMedLDA.h
@@ -1,5 +1,5 @@
 //  HybridMedLDA is an EM-style inference algorithm for MedLDA.
-//		it maximizes the lower bound of evidence.
+//    it maximizes the lower bound of evidence.
 //  E-Step: uses Gibbs sampling to compute Z and \lambda.
 //  M-Step: uses variational inference to infer \eta (Gaussian) and \phi (Dirichlet).
 //
@@ -23,81 +23,79 @@
 
 typedef struct
 {
-	double time, ob_percent, accuracy;
-	double* my;
+  double time, ob_percent, accuracy;
+  double* my;
 }Commit;
 
 class HybridMedLDA {
 public:
-	HybridMedLDA( Corpus* corpus, int category = -1);
-	~HybridMedLDA();
-	
-	// draw samples from posterior.
-	void updateZ(SampleZ* prevZ, CorpusData* dt);
-	void updateLambda( SampleZ* prevZ, CorpusData* dt);
-	void infer_Phi_Eta( SampleZ* prevZ, CorpusData* dt, bool reset);
-	void normalize_Phi_Eta(int N, bool remove);
-	void draw_Z_test(SampleZ* prevZ, int i, CorpusData* dt);
-	void computeZbar(CorpusData* data, SampleZ* Z, int batchIdx);
-	double computeCostFunction(SampleZ* z, CorpusData* dt, int batchIdx, int batchSize);
-	double computeDiscriFunc(CorpusData* dt, int di, Sample* sample, SampleZ* Z, double norm);
-	int eject_sample(CorpusData* dt, SampleZ* Z, int someround); // cancel the effect of mini-batch at some round.
-	
-	// train the model.
-	void init();
-	double train(stl::vec2D<int> batch, stl::vec<int> label);
-	vector<double> inference(vec2D<int> batch, int num_test_sample = -1, int category = -1);
-	
-	
-	
-	// output parameters = samples.
-	deque<Sample*>* samples;
-	deque<SampleZ*>* sampleZs;
-	
-	// input parameters.
-	int K, T, I, J, J_burnin, category, num_category; // J is batch size.
-	Corpus* corpus;
-	
-	// training and testing data.
-	CorpusData *data, *testData;
-	// training data param for convenience.
-	int batchSize, round; // round is the clock of online algo.
-	bool lets_batch, lets_multic; // batch mode.
-	
-	// stats.
-	double** gamma, **prev_gamma; // emperical sum(Od(k,t))
-	double* gammasum; // row sum of gamma.
-	double** eta_icov, **eta_cov, **prev_eta_icov;
-	double* eta_pmean, *eta_mean, *prev_eta_pmean; // mean of eta without transformation.
-	double **stat_phi, **stat_icov, *stat_pmean; // stat used in global update.
-	vector<vector<double> > Zbar_test;
-	vector<vector<double> > Ckt_test;
-	vector<double> Ckt_test_sum;
+  HybridMedLDA( Corpus* corpus, int category = -1);
+  ~HybridMedLDA();
+  
+  // draw samples from posterior.
+  void updateZ(SampleZ* prevZ, CorpusData* dt);
+  void updateLambda( SampleZ* prevZ, CorpusData* dt);
+  void infer_Phi_Eta( SampleZ* prevZ, CorpusData* dt, bool reset);
+  void normalize_Phi_Eta(int N, bool remove);
+  void draw_Z_test(SampleZ* prevZ, int i, CorpusData* dt);
+  void computeZbar(CorpusData* data, SampleZ* Z, int batchIdx);
+  double computeCostFunction(SampleZ* z, CorpusData* dt, int batchIdx, int batchSize);
+  double computeDiscriFunc(CorpusData* dt, int di, Sample* sample, SampleZ* Z, double norm);
+  int eject_sample(CorpusData* dt, SampleZ* Z, int someround); // cancel the effect of mini-batch at some round.
+  
+  // train the model.
+  void init();
+  double train(stl::vec2D<int> batch, stl::vec<int> label);
+  vector<double> inference(vec2D<int> batch, int num_test_sample = -1, int category = -1);
+  
+  
+  
+  // output parameters = samples.
+  deque<Sample*>* samples;
+  deque<SampleZ*>* sampleZs;
+  
+  // input parameters.
+  int K, T, I, J, J_burnin, category, num_category; // J is batch size.
+  Corpus* corpus;
+  
+  // training and testing data.
+  CorpusData *data, *testData;
+  // training data param for convenience.
+  int batchSize, round; // round is the clock of online algo.
+  bool lets_batch, lets_multic; // batch mode.
+  
+  // stats.
+  double** gamma, **prev_gamma; // emperical sum(Od(k,t))
+  double* gammasum; // row sum of gamma.
+  double** eta_icov, **eta_cov, **prev_eta_icov;
+  double* eta_pmean, *eta_mean, *prev_eta_pmean; // mean of eta without transformation.
+  double **stat_phi, **stat_icov, *stat_pmean; // stat used in global update.
+  vector<vector<double> > Zbar_test;
+  vector<vector<double> > Ckt_test;
+  vector<double> Ckt_test_sum;
 
-	int *stat_phi_list_k, *stat_phi_list_t, stat_phi_list_end; // aux stat for sparse update.
-	int *prev_gamma_list_k, *prev_gamma_list_t, prev_gamma_list_end;
-	double *forget_factor; // cumulative forget factor for each round.
-	SampleZ* iZ_test, *iZ;
-	
-	// experiment parameters.
-	double alpha0, beta0, train_time;
-	double c, l, v; // v: prior of eta.
-	deque<int> *batchAlive;
-	int maxSampleN, maxBurninN, max_gibbs_iter, testBurninN;
-	int epoch, batchIdx;
-	double pos_ratio; // ratio of positive examples in the training set.
-	double u, tao; // gradient contribution.
-	bool mode_additive; // use sequential bayesian experiment mode.
-	// aux samplers.
-	InverseGaussian* invgSampler;
-	MVGaussian* mvGaussianSampler;
-	objcokus cokus;
-		
-	// result.
-	double test_acc;
-	vector<Commit> commit_points;
-	bool lets_commit;
-	int commit_point_spacing, commit_point_n;
+  int *stat_phi_list_k, *stat_phi_list_t, stat_phi_list_end; // aux stat for sparse update.
+  int *prev_gamma_list_k, *prev_gamma_list_t, prev_gamma_list_end;
+  
+  // experiment parameters.
+  double alpha0, beta0, train_time;
+  double c, l, v; // v: prior of eta.
+  deque<int> *batchAlive;
+  int maxSampleN, maxBurninN, max_gibbs_iter, testBurninN;
+  int epoch, batchIdx;
+  double pos_ratio; // ratio of positive examples in the training set.
+  double u, tao; // gradient contribution.
+  bool mode_additive; // use sequential bayesian experiment mode.
+  // aux samplers.
+  InverseGaussian* invgSampler;
+  MVGaussian* mvGaussianSampler;
+  objcokus cokus;
+    
+  // result.
+  double test_acc;
+  vector<Commit> commit_points;
+  bool lets_commit;
+  int commit_point_spacing, commit_point_n;
 
 };
 
--- OnlineGibbsMedLDA/src/paMedLDAgibbsWrapper.cpp
+++ OnlineGibbsMedLDA/src/paMedLDAgibbsWrapper.cpp
@@ -5,24 +5,24 @@ using namespace std;
 
 paMedLDAgibbsWrapper::paMedLDAgibbsWrapper(boost::python::dict config) 
 {
-	string train_file = bp::extract<string>(config["train_file"]);
-	string test_file = bp::extract<string>(config["test_file"]);
-	corpus = shared_ptr<Corpus>(new Corpus());
-	corpus->loadDataGML(train_file, test_file);
-	pamedlda.resize(corpus->newsgroupN);
-	for(int ci = 0; ci < corpus->newsgroupN; ci++) {
-		pamedlda[ci] = shared_ptr<HybridMedLDA>(new HybridMedLDA(&*corpus, ci));
-		pamedlda[ci]->K = bp::extract<int>(config["num_topic"]);
-		pamedlda[ci]->batchSize = bp::extract<int>(config["batchsize"]);
-		pamedlda[ci]->lets_multic = true;
-		pamedlda[ci]->alpha0 = bp::extract<float>(config["alpha"]);
-		pamedlda[ci]->beta0 = bp::extract<float>(config["beta"]);
-		pamedlda[ci]->c = bp::extract<float>(config["c"]);
-		pamedlda[ci]->l = bp::extract<float>(config["l"]);
-		pamedlda[ci]->I = bp::extract<int>(config["I"]);
-		pamedlda[ci]->J = bp::extract<int>(config["J"]);
-		pamedlda[ci]->init();
-	}
+  string train_file = bp::extract<string>(config["train_file"]);
+  string test_file = bp::extract<string>(config["test_file"]);
+  corpus = shared_ptr<Corpus>(new Corpus());
+  corpus->loadDataGML(train_file, test_file);
+  pamedlda.resize(corpus->newsgroupN);
+  for(int ci = 0; ci < corpus->newsgroupN; ci++) {
+    pamedlda[ci] = shared_ptr<HybridMedLDA>(new HybridMedLDA(&*corpus, ci));
+    pamedlda[ci]->K = bp::extract<int>(config["num_topic"]);
+    pamedlda[ci]->batchSize = bp::extract<int>(config["batchsize"]);
+    pamedlda[ci]->lets_multic = true;
+    pamedlda[ci]->alpha0 = bp::extract<float>(config["alpha"]);
+    pamedlda[ci]->beta0 = bp::extract<float>(config["beta"]);
+    pamedlda[ci]->c = bp::extract<float>(config["c"]);
+    pamedlda[ci]->l = bp::extract<float>(config["l"]);
+    pamedlda[ci]->I = bp::extract<int>(config["I"]);
+    pamedlda[ci]->J = bp::extract<int>(config["J"]);
+    pamedlda[ci]->init();
+  }
 }
 
 paMedLDAgibbsWrapper::~paMedLDAgibbsWrapper() 
@@ -32,150 +32,150 @@ paMedLDAgibbsWrapper::~paMedLDAgibbsWrapper()
 
 
 void paMedLDAgibbsWrapper::train(bp::list batch, bp::list label) {
-	auto filtered = filterWordAndLabel(batch, label, _numWord(), _numLabel());
-	int num_category = pamedlda[0]->num_category;
-	vector<thread> threads(num_category);
-	for(int ci = 0; ci < num_category; ci++) {
-		threads[ci] = std::thread([&](int id)  {
-			pamedlda[id]->train(filtered.first, filtered.second);
-		}, ci);
-	}
-	for(int ci = 0; ci < num_category; ci++) threads[ci].join();
+  auto filtered = filterWordAndLabel(batch, label, _numWord(), _numLabel());
+  int num_category = pamedlda[0]->num_category;
+  vector<thread> threads(num_category);
+  for(int ci = 0; ci < num_category; ci++) {
+    threads[ci] = std::thread([&](int id)  {
+      pamedlda[id]->train(filtered.first, filtered.second);
+    }, ci);
+  }
+  for(int ci = 0; ci < num_category; ci++) threads[ci].join();
 }
 
 bp::list paMedLDAgibbsWrapper::infer(bp::list batch, bp::list label, boost::python::object num_test_sample) {
-	int num_category = pamedlda[0]->num_category;
-	auto filtered = filterWordAndLabel(batch, label, _numWord(), _numLabel());
-	auto docs = filtered.first;
-	auto labels = filtered.second;
-	vector<thread> threads(num_category);
-	vector<vector<double> > my(num_category);
-	for(int ci = 0; ci < num_category; ci++) {
-		threads[ci] = std::thread([&](int id)  {
-			my[id] = pamedlda[id]->inference(docs, bp::extract<int>(num_test_sample));		
-		}, ci);
-	}
-	for(int ci = 0; ci < num_category; ci++) threads[ci].join();	
-	double acc = 0;
-	bp::list ret;
-	for(size_t d = 0; d < labels->size(); d++) {
-		int output = 0;
-		double confidence = 0-INFINITY;
-		for( int ci = 0; ci < num_category; ci++) {
-			if(my[ci][d] > confidence) {
-				output = ci;
-				confidence = pamedlda[ci]->testData->my[d];
-			}
-		}
-		ret.append(output);
-		if(output == (*labels)[d]) {
-			acc++;
-		}
-	}
-	m_test_acc = (double)acc/(double)labels->size();
-	return ret;
+  int num_category = pamedlda[0]->num_category;
+  auto filtered = filterWordAndLabel(batch, label, _numWord(), _numLabel());
+  auto docs = filtered.first;
+  auto labels = filtered.second;
+  vector<thread> threads(num_category);
+  vector<vector<double> > my(num_category);
+  for(int ci = 0; ci < num_category; ci++) {
+    threads[ci] = std::thread([&](int id)  {
+      my[id] = pamedlda[id]->inference(docs, bp::extract<int>(num_test_sample));    
+    }, ci);
+  }
+  for(int ci = 0; ci < num_category; ci++) threads[ci].join();  
+  double acc = 0;
+  bp::list ret;
+  for(size_t d = 0; d < labels->size(); d++) {
+    int output = 0;
+    double confidence = 0-INFINITY;
+    for( int ci = 0; ci < num_category; ci++) {
+      if(my[ci][d] > confidence) {
+        output = ci;
+        confidence = pamedlda[ci]->testData->my[d];
+      }
+    }
+    ret.append(output);
+    if(output == (*labels)[d]) {
+      acc++;
+    }
+  }
+  m_test_acc = (double)acc/(double)labels->size();
+  return ret;
 }
 
 bp::object paMedLDAgibbsWrapper::timeElapsed() const {
-	double train_time = 0;
-	for(auto t : pamedlda) {
-		train_time += t->train_time;
-	}
-	return bp::object(train_time/(double)corpus->newsgroupN);
+  double train_time = 0;
+  for(auto t : pamedlda) {
+    train_time += t->train_time;
+  }
+  return bp::object(train_time/(double)corpus->newsgroupN);
 }
 
 bp::list paMedLDAgibbsWrapper::topicMatrix(bp::object category_no) const {
-	int ci = bp::extract<int>(category_no);
-	bp::list mat;
-	for(int k = 0; k < this->pamedlda[ci]->K; k++) {
-		bp::list row;
-		for(int t = 0; t < this->pamedlda[ci]->T; t++) {
-			row.append(pamedlda[ci]->gamma[k][t]/(double)pamedlda[ci]->gammasum[k]);
-		}
-		mat.append(row);
-	}
-	return mat;
+  int ci = bp::extract<int>(category_no);
+  bp::list mat;
+  for(int k = 0; k < this->pamedlda[ci]->K; k++) {
+    bp::list row;
+    for(int t = 0; t < this->pamedlda[ci]->T; t++) {
+      row.append(pamedlda[ci]->gamma[k][t]/(double)pamedlda[ci]->gammasum[k]);
+    }
+    mat.append(row);
+  }
+  return mat;
 }
 
 bp::list paMedLDAgibbsWrapper::topWords(bp::object category_no, int topk) const {
-	int ci = bp::extract<int>(category_no);
-	bp::list mat;
-	for(int k = 0; k < this->pamedlda[ci]->K; k++) {
-		vector<sortable> v;
-		for(int t = 0; t < this->pamedlda[ci]->T; t++) {
-			sortable x;
-			x.value = pamedlda[ci]->gamma[k][t]/(double)pamedlda[ci]->gammasum[k];
-			x.index = t;
-			v.push_back(x);
-		}
-		sort(v.begin(), v.end());
-		bp::list row;
-		for(int i = 0; i < topk; i++) {
-			row.append(v[i].index);
-		}
-		mat.append(row);
-	}
-	return mat;	
+  int ci = bp::extract<int>(category_no);
+  bp::list mat;
+  for(int k = 0; k < this->pamedlda[ci]->K; k++) {
+    vector<sortable> v;
+    for(int t = 0; t < this->pamedlda[ci]->T; t++) {
+      sortable x;
+      x.value = pamedlda[ci]->gamma[k][t]/(double)pamedlda[ci]->gammasum[k];
+      x.index = t;
+      v.push_back(x);
+    }
+    sort(v.begin(), v.end());
+    bp::list row;
+    for(int i = 0; i < topk; i++) {
+      row.append(v[i].index);
+    }
+    mat.append(row);
+  }
+  return mat;  
 }
 
 bp::list paMedLDAgibbsWrapper::topicDistOfInference(bp::object category_no) const {
-	int ci = bp::extract<int>(category_no);
-	bp::list mat;
-	for(const vector<double>& zbar_row : this->pamedlda[ci]->Zbar_test) {
-		bp::list row;
-		for(const double& zbar_entry : zbar_row) {
-			row.append(zbar_entry);
-		}
-		mat.append(row);
-	}
-	return mat;
+  int ci = bp::extract<int>(category_no);
+  bp::list mat;
+  for(const vector<double>& zbar_row : this->pamedlda[ci]->Zbar_test) {
+    bp::list row;
+    for(const double& zbar_entry : zbar_row) {
+      row.append(zbar_entry);
+    }
+    mat.append(row);
+  }
+  return mat;
 }
 
 bp::list paMedLDAgibbsWrapper::labelOfInference() const {
-	bp::list list;
-	for(int d = 0; d < this->corpus->testDataSize; d++) {
-		list.append(corpus->testData[d]->label);
-	}
-	return list;
+  bp::list list;
+  for(int d = 0; d < this->corpus->testDataSize; d++) {
+    list.append(corpus->testData[d]->label);
+  }
+  return list;
 }
 
 inline bp::object paMedLDAgibbsWrapper::numWord() const {
-	return bp::object(_numWord());
+  return bp::object(_numWord());
 }
 
 inline bp::object paMedLDAgibbsWrapper::numLabel() const {
-	return bp::object(_numLabel());
+  return bp::object(_numLabel());
 }
 
 inline size_t paMedLDAgibbsWrapper::_numWord() const {
-	return pamedlda[0]->T;
+  return pamedlda[0]->T;
 }
 
 inline size_t paMedLDAgibbsWrapper::_numLabel() const {
-	return pamedlda[0]->num_category;
+  return pamedlda[0]->num_category;
 }
 
 ////////////////////////////////////////////////////////
 //////////// private methods /////////////////////////
 pair<vec2D<int>, vec<int> > 
 paMedLDAgibbsWrapper::filterWordAndLabel(bp::list batch, bp::list label, size_t T, size_t C) {
-	auto ret = makeVector2D<int>();
-	auto new_label = makeVector<int>();
-	for(size_t ni = 0; ni < bp::len(batch); ni++) {
-		bp::list ex = bp::extract<bp::list>(batch[ni]);
-		size_t y = (size_t)bp::extract<int>(label[ni]);
-		if(y >= C) continue;
-		std::vector<int> row;
-		for(size_t wi = 0; wi < bp::len(ex); wi++) {
-			size_t token = (size_t)bp::extract<int>(ex[wi]);
-			if(token < T) {
-				row.push_back(token);
-			}
-		}
-		ret->push_back(row);
-		new_label->push_back(y);
-	}
-	return make_pair(ret, new_label);
+  auto ret = makeVector2D<int>();
+  auto new_label = makeVector<int>();
+  for(size_t ni = 0; ni < bp::len(batch); ni++) {
+    bp::list ex = bp::extract<bp::list>(batch[ni]);
+    size_t y = (size_t)bp::extract<int>(label[ni]);
+    if(y >= C) continue;
+    std::vector<int> row;
+    for(size_t wi = 0; wi < bp::len(ex); wi++) {
+      size_t token = (size_t)bp::extract<int>(ex[wi]);
+      if(token < T) {
+        row.push_back(token);
+      }
+    }
+    ret->push_back(row);
+    new_label->push_back(y);
+  }
+  return make_pair(ret, new_label);
 }
 
 BOOST_PYTHON_MODULE(libbayespagibbs)
